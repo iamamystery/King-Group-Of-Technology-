e3806c602df15342b0b9e5afd283535569e0e24f
iamamystery <jawadking6971@gmail.com>
2026-02-27 08:56:42 +0500

    Remove glow from logo

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3Wq5pExF7XKo3HSJQQQZfQ5zApD8jwKwurZzoZsgYg8=\"\n}"
--- dist/dev/static/chunks/app_error_tsx_7d9c702b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_08fbd3ea._.js",
-  "static/chunks/_77139774._.js"
-],
-    source: "dynamic"
-});
